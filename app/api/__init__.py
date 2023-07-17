363f1c13034bca727f69126a39d347260a9c7ca6
Rumiia Sinelnikova <112545996+RumKam@users.noreply.github.com>
2023-07-17 22:27:52 +0300

    Delete __init__.py

--- app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.model import order_model_router
-
-
-api_router = APIRouter(prefix='/api/v1')
-
-# Подключение роутов
-api_router.include_router(order_model_router)
